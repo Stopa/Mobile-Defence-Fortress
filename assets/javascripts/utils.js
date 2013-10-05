ae8f476f0f51d3c495e093dae1fee31d48f356b6
Jakob Mass <jaks@ut.ee>
2013-10-05 04:02:13 +0300

    Improved debugging rectangle util

--- assets/javascripts/utils.js
+++ assets/javascripts/utils.js
@@ -10,17 +10,50 @@ MDF.angleSpeeds = function(angle) {
     };
 }
 
-MDF.createDebugRect = function (object) {
-	//draw a rectangle which based on objects width and height
-	//for 'visual debugging'
-        if (Game.debug){
-            object.shape = new createjs.Shape();
-            object.shape.x = 0;
-            object.shape.y = 0;
-            object.addChild(object.shape);
-        }
-}
+/* 
+*   Draws a rectangle around the argument object,
+*   based on objects width and height. for 'visual debugging'
+*   @param container - the container which shall hold the rectangle as a child (by default: the arg object itself)
+*/
+MDF.createDebugRect = function (object, color, container) {
+    if (Game.debug){
+        object.box = new createjs.Shape();
+        
+        object.box.color = (color !== undefined) ? color : "#F00";
+        object.box.width = object.width;
+        object.box.height = object.height;
+        //object.box.regX = object.regX;
+        object.box.regY = object.regY;
 
+        if (container !== undefined) {
+            object.box.container = container;
+            container.addChild(object.box);
+        } else {
+            object.addChild(object.box);
+        }
+    }
+}
+/*
+* Clear and (re)draw the debug rectangle (for example, call during every tick)
+*/
 MDF.updateDebugRect = function (object) {
-	object.shape.graphics.clear().beginStroke("#F00").rect(0,0,object.width,object.height);
+    if (Game.debug){
+        var rectX = 0;
+        var rectY = 0;
+
+        //if the container is something other than the object itself,
+        // we need to take that into account
+        if (object.box.container !== undefined){
+            rectX = object.x;
+            rectY = object.y;
+        }
+    	object.box.graphics.clear();
+        object.box.graphics.beginStroke(object.box.color);
+        object.box.graphics.rect(
+            rectX,
+            rectY,
+            object.box.width,
+            object.box.height
+        );
+    }
 }
