e67fbae325fc6eef946501499a2880a9f5f91671
Stepan Bolotnikov <stepan@fraktal.ee>
2013-10-10 01:49:42 +0300

    Limited Enemies' and Swarms' movement along the Y axis

--- assets/javascripts/Swarm.js
+++ assets/javascripts/Swarm.js
@@ -74,15 +74,16 @@
         this.x += this.curDirection * xSpeed;
         if (this.x < 0 + this.regX || this.x >= (Game.canvasWidth - this.regX) ){
             this.curDirection *=-1;
-            this.y += ySpeed;
+            console.log(this.y, Stage.canvas.clientHeight*0.5)
+            if(this.y < Stage.canvas.clientHeight*0.5) {
+                this.y += ySpeed;
+            }
         }
 
         if (Math.floor(Math.random()*1000) % 50 == 0) {
             this.children[Math.floor(Math.random()*this.children.length)].dropBomb(); 
             //!TODO : using this.children also tries to call dropBomb() from the debugrect
         }
-
-        //!TODO: Handle land approaching
     }
     
     window.Swarm = Swarm;
