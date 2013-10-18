34a41f87de12244f0bd2e590aedb7211aed85e6c
Taavi Kala <xenover@gmail.com>
2013-10-18 23:11:03 +0300

    Add basic player tests

--- assets/javascripts/test_main.js
+++ assets/javascripts/test_main.js
@@ -13,25 +13,36 @@ const KEYS =  {
 var Game, Player
 
 Game = function() {
-    return {
-        init: function() {
-          module("Player")
-          test("Player baseHitpoints initialization without arguments", function() {
-            Player = new Player();
-            equal(Player.baseHitpoints, 100, "Player's baseHitpoints are equal to 100 on argumentless initialization");
-          });
-        },
-        controls: {
-            movementKeyPressed: undefined,
-            leftMouseDown: false,
-            rightMouseDown: false
-        },
-        factions: {
-            humans: 0,
-            aliens: 1
-        },
-        colliders : [],
-        debug : false,
-        canvasWidth : 100
-    }
+  return {
+    init: function() {
+      module("Player")
+      Player = new Player();
+      test("BaseHitpoints initialization", function() {
+        equal(Player.baseHitpoints, 100, "Player's BaseHitpoints are equal to 100");
+      });
+      test("Damage taking", function() {
+        Player.takesDamage(20);
+        equal(Player.baseHitpoints, 80, "Taking 20 damage reduced Player's baseHitpoints to 80");
+      });
+      // test("Player's death", function() {
+      // Player.takesDamage(81);
+      // equal(Player.parent.hasChild(Player), false, "Player's baseHitpoints were reduced to a negative value and player died");
+      // });  ---  No idea how to check if player died
+      test("Player's faction", function() {
+        equal(Player.faction, Game.factions.humans, "Player belongs to the 'humans' faction");
+      });
+    },
+    controls: {
+        movementKeyPressed: undefined,
+        leftMouseDown: false,
+        rightMouseDown: false
+    },
+    factions: {
+        humans: 0,
+        aliens: 1
+    },
+    colliders : [],
+    debug : false,
+    canvasWidth : 100
+  }
 }();
