10f47659a3fa378e33a20ed67d7ff374ee25cb27
Taavi Kala <xenover@gmail.com>
2013-10-21 22:46:54 +0300

    Dynamic death check, swarm ship count tests

--- assets/javascripts/test_main.js
+++ assets/javascripts/test_main.js
@@ -10,40 +10,52 @@ const KEYS =  {
     RIGHT: 1
 }
 
-var Game, Player, Enemy
+var Game, Stage, player, enemy, Swarm
 
 Game = function() {
+  Stage = new createjs.Stage('mainCanvas');
+
   return {
     init: function() {
       module("Player")
-      Player = new Player();
+      player = new Player();
       test("BaseHitpoints initialization", function() {
-        equal(Player.baseHitpoints, 100, "Player's BaseHitpoints are equal to 100");
+        equal(player.baseHitpoints, 100, "Player's BaseHitpoints are equal to 100");
       });
       test("Faction", function() {
-        equal(Player.faction, Game.factions.humans, "Player belongs to the 'humans' faction");
+        equal(player.faction, Game.factions.humans, "Player belongs to the 'humans' faction");
       });
       test("Damage taking", function() {
-        Player.takesDamage(20);
-        equal(Player.baseHitpoints, 80, "Taking 20 damage reduced Player's baseHitpoints to 80");
+        player.takesDamage(20);
+        equal(player.baseHitpoints, 80, "Taking 20 damage reduced Player's baseHitpoints to 80");
       });
-      test("Player's death", function() {
-      Player.takesDamage(81);
-      ok(Player.baseHitpoints < 0, "Player is dead");
+      test("Death", function() {
+        player.takesDamage(81);
+        equal(Stage.children.indexOf(player), -1, "Player is dead");
       });
       
       module("Enemy");
-      Enemy = new Enemy();
+      enemy = new Enemy();
       test("BaseHitpoints initialization", function() {
-        equal(Enemy.baseHitpoints, 100, "Enemy's BaseHitpoints are equal to 100");
+        equal(enemy.baseHitpoints, 100, "Enemy's BaseHitpoints are equal to 100");
       });
       test("Faction", function() {
-        equal(Enemy.faction, Game.factions.aliens, "Enemy belongs to the 'aliens' faction");
+        equal(enemy.faction, Game.factions.aliens, "Enemy belongs to the 'aliens' faction");
       });
       test("Death on single collision", function() {
-        Enemy.collision({});
-        ok(Enemy.baseHitpoints < 0, "Enemy is dead")
+        enemy.collision({});
+        ok(enemy.baseHitpoints < 0, "Enemy is dead")
       });
+
+      module("Swarm");
+      swarm = new Swarm(0,0,2,3,0);
+      test("Ships count by totalShips", function() {
+        equal(swarm.totalShips, 2 * 3, "Swarm's totalShips variable is 6");
+      });
+      test("Ships count by array size", function() {
+        equal(swarm.shipsArray.length, 2 * 3, "Swarm's shipsArray size is 6")
+      });
+
     },
     controls: {
         movementKeyPressed: undefined,
