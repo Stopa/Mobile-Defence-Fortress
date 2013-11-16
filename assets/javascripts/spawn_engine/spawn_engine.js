32aeada1f69169863dfa15013886f597f7d40f73
Henry Mägi <henrymagi@live.com>
2013-11-16 19:01:10 +0200

    Updated Spawn Engine
    New Swarm formation methods

--- assets/javascripts/spawn_engine/spawn_engine.js
+++ assets/javascripts/spawn_engine/spawn_engine.js
@@ -67,12 +67,9 @@ function spawnWaves() {
     var spawn = {
       x : Game.transformedSize.x/2,
       y : 200,
-      rows : swarmsArray[i][1],
-      cols : swarmsArray[i][2],
-      horizontalPadding : 50,
       ticksLeft: swarmsArray[i][3]*60 // Ticks per second = 60
     }
-    
+
     spawns.push(spawn);
  		spawnedSwarms++;
 	}
@@ -82,10 +79,8 @@ function getWaves() {
 	var swarmsArray = new Array();
 	for (var i = 1; i <= swarmCount; i++) {
 		var swarmType = '#swarm_' + i + ' .form-group:nth-child(1) option:selected';
-		var swarmRows = '#swarm_' + i + ' .form-group:nth-child(2) input';
-		var swarmColumns = '#swarm_' + i + ' .form-group:nth-child(3) input';
 		var swarmTimer = '#swarm_' + i + ' .form-group:nth-child(4) input';
-		var swarm = [$(swarmType).text(), $(swarmRows).val(), $(swarmColumns).val(), $(swarmTimer).val()];
+		var swarm = [$(swarmType).text(), $(swarmTimer).val()];
 		swarmsArray.push(swarm);
 	}
 	return swarmsArray;
@@ -95,7 +90,7 @@ function SpawnEngineTick() {
   for(var i = spawns.length; i > 0; i--) {
     if(spawns[i-1].ticksLeft <= 0) {
       var spawn = spawns[i-1];
-      Game.gameArea.addChild(new Swarm(spawn.x, spawn.y, spawn.rows, spawn.cols, spawn.horizontalPadding));
+      Game.gameArea.addChild(new FormationClassicF1(spawn.x, spawn.y));
       spawns.splice(i-1,1);
     } else {
       spawns[i-1].ticksLeft--;
